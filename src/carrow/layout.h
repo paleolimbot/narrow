f33193d86e11060b22c12c832c3277902eff22b9
Dewey Dunnington <dewey@fishandwhistle.net>
2021-09-04 11:15:50 -0300

    start on carrow stuff

---
+++ src/carrow/layout.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <stdint.h>
+#include "static-copy.h"
+
+struct ArrowBufferType {
+  char identifier;
+  int buffer_type;
+  uint64_t el_size;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
