23d62ba635337e5caba805a380215eccfe54c269
Dewey Dunnington <dewey@fishandwhistle.net>
2021-11-16 10:59:36 -0400

    write some tests and reminders for stuff that doesn't work yet

--- tests/testthat/test-function.R
+++ tests/testthat/test-function.R
@@ -1,37 +1,57 @@
 
 test_that("arrow_identity() works for primitive types", {
+  # primitive with no NAs (no validity buffer)
   expect_identical(
     from_arrow_vctr(arrow_identity(as_arrow_vctr(1:5)), integer()),
     1:5
   )
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(as.numeric(1:5))), double()),
+    as.numeric(1:5)
+  )
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(c(TRUE, FALSE))), logical()),
+    c(TRUE, FALSE)
+  )
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(as.raw(1:5))), raw()),
+    as.raw(1:5)
+  )
 
-  if (FALSE) {
-    arrow_identity(as_arrow_vctr(as.numeric(1:5)))
-    arrow_identity(as_arrow_vctr(as.raw(1:5)))
-    arrow_identity(as_arrow_vctr(c(TRUE, FALSE)))
-
-    arrow_identity(as_arrow_vctr(c(NA, 1:5)))
-    arrow_identity(as_arrow_vctr(as.numeric(c(NA, 1:5))))
-    arrow_identity(as_arrow_vctr(c(NA, TRUE, FALSE)))
-  }
-})
-
-test_that("arrow_identity() works for structs", {
-  # these segfault currently
-  if (FALSE) {
-    df <- data.frame(
-      a = as_arrow_vctr(1:5),
-      b = as_arrow_vctr(as.numeric(1:5)),
-      c = as_arrow_vctr(as.raw(1:5))
-    )
-
-    arrow_identity(df)
-  }
+  skip("validity buffer isn't copied yet")
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(c(NA, 1:5))), integer()),
+    c(NA, 1:5)
+  )
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(as.numeric(c(NA, 1:5)))), double()),
+    as.numeric(c(NA, 1:5))
+  )
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(c(TRUE, FALSE, NA))), logical()),
+    c(TRUE, FALSE, NA)
+  )
 })
 
 test_that("arrow_identity() works for character", {
-  # these segfault currently
-  if (FALSE) {
-    arrow_identity(c("one", "two", "three", "four"))
-  }
+  skip("arrow_identity() doesn't work for character")
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(c("one", "two", "three", "four")))),
+    c("one", "two", "three", "four")
+  )
+})
+
+test_that("arrow_identity() works for structs", {
+  skip("arrow_identity() doesn't work for structs")
+  df <- data.frame(
+    a = 1:5,
+    b = as.numeric(1:5),
+    c = as.raw(1:5),
+    d = rep_len(c(TRUE, FALSE), 5)
+  )
+
+  expect_identical(
+    from_arrow_vctr(arrow_identity(as_arrow_vctr(df)), ptype = df),
+    df
+  )
 })
