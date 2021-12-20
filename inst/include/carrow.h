d59605bb66a5f9a252d8025a52997e17e3df25e7
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-20 09:51:41 -0400

    add array stream from xptr interface

--- inst/include/carrow.h
+++ inst/include/carrow.h
@@ -133,6 +133,23 @@ static inline struct ArrowArray* array_data_from_xptr(SEXP array_data_xptr, cons
   return array_data;
 }
 
+static inline struct ArrowArrayStream* array_stream_from_xptr(SEXP array_stream_xptr, const char* arg) {
+  if (!Rf_inherits(array_stream_xptr, "carrow_array_stream")) {
+    Rf_error("`%s` must be an object created with carrow_array_stream()", arg);
+  }
+
+  struct ArrowArrayStream* array_stream = (struct ArrowArrayStream*) R_ExternalPtrAddr(array_stream_xptr);
+  if (array_stream == NULL) {
+    Rf_error("`%s` is an external pointer to NULL", arg); // # nocov
+  }
+
+  if (array_stream->release == NULL) {
+    Rf_error("`%s` has already been released and is no longer valid", arg); // # nocov
+  }
+
+  return array_stream;
+}
+
 #ifdef __cplusplus
 }
 #endif
